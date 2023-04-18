3975b0985ed9a20ae8e780a7cda4cf6b883cba02
Samantha Alejandre <alejandresam@g.ucla.edu>
2023-04-18 16:35:42 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title('title of your application')
+st.markdown('for example here is a **bold text**')
+
+st.sidebar.title('title of the sidebar')
+
+agree = st.checkbox('Click me')
+if agree:
+    st.write('Great')
+    st.markdown('this is *italic text*')
+
+side_check = st.sidebar.checkbox('Click Checkbox')
+if side_check:
+    st.sidebar.write('Sidebar checkbox has been clicked')
