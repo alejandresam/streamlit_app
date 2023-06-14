e14487f790d27968a66746f58d041e33b0c90132
Samantha Alejandre <alejandresam@g.ucla.edu>
2023-06-13 21:08:52 -0700

    final commit

--- app.py
+++ app.py
@@ -2,48 +2,9 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import base64
 
-web_apps = st.sidebar.selectbox("Select Web Apps", ("Exploratory Data Analysis", "Distributions"))
 
-if web_apps == "Exploratory Data Analysis":
-    uploaded_file = st.sidebar.file_uploader("Choose a file")
-
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
-        show_df = st.checkbox("Show Data Frame", key="disabled")
-
-        if show_df:
-            st.write(df)
-
-        column_type = st.sidebar.selectbox('Select Data Type', ("Numerical", "Categorical", "Bool", "Date"))
-
-        if column_type == "Numerical":
-            numerical_column = st.sidebar.selectbox('Select a Column', df.select_dtypes(include=['int64', 'float64']).columns)
-
-            # Histogram
-            st.subheader("Histogram")
-            hist_bins = st.slider('Number of Bins', min_value=5, max_value=150, value=30)
-            hist_title = st.text_input('Set Title', 'Histogram')
-            hist_xtitle = st.text_input('Set x-axis Title', numerical_column)
-
-            fig, ax = plt.subplots()
-            ax.hist(df[numerical_column], bins=hist_bins, edgecolor="black")
-            ax.set_title(hist_title)
-            ax.set_xlabel(hist_xtitle)
-            ax.set_ylabel('Count')
-
-            st.pyplot(fig)
-
-            # Download the plot
-            st.subheader("Download Plot")
-            download_button = st.button("Download Histogram Plot")
-
-            if download_button:
-                filename = f"{numerical_column}_histogram.png"
-                fig.savefig(filename, dpi=300)
-                st.download_button(label="Download", data=filename, file_name=filename)
-
-# Function to display dataset statistics
 def display_dataset_stats(df):
     num_rows = len(df)
     num_cols = len(df.columns)
@@ -58,15 +19,12 @@ def display_dataset_stats(df):
     st.write(f"Number of boolean variables: {num_bool}")
 
 
-# Function to display five number summary and distribution plot
 def display_numerical_stats(df, selected_column):
     st.subheader("Numerical Column Statistics")
 
-    # Five number summary
     five_num_summary = df[selected_column].describe()
     st.write(five_num_summary)
 
-    # Distribution plot
     fig, ax = plt.subplots()
     sns.histplot(data=df, x=selected_column, kde=True)
     ax.set_title("Distribution Plot")
@@ -74,16 +32,16 @@ def display_numerical_stats(df, selected_column):
     ax.set_ylabel("Frequency")
     st.pyplot(fig)
 
+    # Save the plot as a PNG file
+    save_plot_as_png(fig, "plot.png")
 
-# Function to display categorical proportions and bar plot
+
 def display_categorical_stats(df, selected_column):
     st.subheader("Categorical Column Statistics")
 
-    # Proportions of each category level
     category_proportions = df[selected_column].value_counts(normalize=True)
     st.write(category_proportions)
 
-    # Customized bar plot
     fig, ax = plt.subplots()
     sns.countplot(data=df, x=selected_column)
     ax.set_title("Bar Plot")
@@ -91,28 +49,89 @@ def display_categorical_stats(df, selected_column):
     ax.set_ylabel("Count")
     st.pyplot(fig)
 
+    # Save the plot as a PNG file
+    save_plot_as_png(fig, "plot.png")
+
+
+def save_plot_as_png(fig, filename):
+    fig.savefig(filename, dpi=300)
+
+
+def download_file(df, filename):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">Download CSV File</a>'
+    st.markdown(href, unsafe_allow_html=True)
+
 
 def main():
     st.title("Exploratory Data Analysis (EDA)")
 
-    uploaded_file = st.file_uploader("Choose a CSV file")
+    web_apps = st.sidebar.selectbox("Select Web Apps", ("Exploratory Data Analysis",))
 
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
+    if web_apps == "Exploratory Data Analysis":
+        uploaded_file = st.sidebar.file_uploader("Choose a CSV file")
 
-        # Display dataset statistics
-        st.subheader("Dataset Statistics")
-        display_dataset_stats(df)
+        if uploaded_file is not None:
+            try:
+                df = pd.read_csv(uploaded_file)
 
-        # Select a column from the dataset
-        selected_column = st.selectbox("Select a column", df.columns)
+                st.sidebar.subheader("Navigation")
+                page = st.sidebar.radio("Go to", ("Dataset Stats", "Numerical Stats", "Categorical Stats"))
 
-        if df[selected_column].dtype == "object":
-            # Categorical column
-            display_categorical_stats(df, selected_column)
-        else:
-            # Numerical column
-            display_numerical_stats(df, selected_column)
+                if page == "Dataset Stats":
+                    st.subheader("Dataset Statistics")
+                    display_dataset_stats(df)
+
+                elif page == "Numerical Stats":
+                    numerical_columns = df.select_dtypes(include=['int64', 'float64']).columns
+
+                    if len(numerical_columns) > 0:
+                        selected_column = st.selectbox("Select a column", numerical_columns)
+                        display_numerical_stats(df, selected_column)
+
+                        # Download the plot
+                        st.sidebar.markdown("---")
+                        st.sidebar.subheader("Download Plot Image")
+                        download_button = st.sidebar.button("Download Plot")
+                        if download_button:
+                            save_plot_as_png(plt.gcf(), "plot.png")
+                            st.sidebar.markdown(get_download_link("plot.png", "Download Plot"), unsafe_allow_html=True)
+                    else:
+                        st.write("No numerical columns found in the dataset.")
+
+                elif page == "Categorical Stats":
+                    categorical_columns = df.select_dtypes(include=['object']).columns
+
+                    if len(categorical_columns) > 0:
+                        selected_column = st.selectbox("Select a column", categorical_columns)
+                        display_categorical_stats(df, selected_column)
+
+                        # Download the plot
+                        st.sidebar.markdown("---")
+                        st.sidebar.subheader("Download Plot Image")
+                        download_button = st.sidebar.button("Download Plot")
+                        if download_button:
+                            save_plot_as_png(plt.gcf(), "plot.png")
+                            st.sidebar.markdown(get_download_link("plot.png", "Download Plot"), unsafe_allow_html=True)
+                    else:
+                        st.write("No categorical columns found in the dataset.")
+
+            except pd.errors.ParserError:
+                st.write("Invalid CSV file. Please upload a valid CSV file.")
+
+    st.sidebar.markdown("---")
+    st.sidebar.subheader("Download Processed Data")
+    if 'df' in locals():
+        download_file(df, "processed_data.csv")
+
+
+def get_download_link(file_path, link_text):
+    with open(file_path, "rb") as file:
+        contents = file.read()
+        b64 = base64.b64encode(contents).decode()
+        href = f'<a href="data:file/png;base64,{b64}" download="{file_path}">{link_text}</a>'
+        return href
 
 
 if __name__ == "__main__":
